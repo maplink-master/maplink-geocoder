b4dd64ebcc672ba55f3d1ea44614eb5b4ec22b86
Filipe Silva <filipe.silva@maplink.com.br>
2017-05-22 11:17:43 -0300

    fix test and added new one to ping at dns api.maplink.com.br

--- test/geocoder-test.js
+++ test/geocoder-test.js
@@ -1,19 +1,29 @@
 const chai = require("chai"),
-	geocoder = require("../src")
+	geocoder = require("../src"),
+	ping = require ("ping")
 
 let expect = chai.expect
+let assert = chai.assert
 
 describe("geocoder", () => {
 	describe("constructor", () => {
 		it("Should construct object when sent clientKey and clientSecret", () => {
-			let geo = geocoder("user", "secretPassword")
+			let geo = geocoder("secretPassword", "user")
 
 			expect(geo).to.have.property("search").with.length.above(0)
-			expect(geo).to.have.property("geocode").with.length.above(0)
+			expect(geo).to.have.property("structured").with.length.above(0)
 		})
 
 		it("Should return an error if not sent clientKey and clientSecret", () => {
-			expect(_ => geocoder()).to.throw(Error, "You must to send clientKey and clientSecret.")
+			expect(_ => geocoder()).to.throw(Error, /You must to send clientKey and clientSecret/)
+		})
+	})
+	describe("ping api maplink", () => {
+		it("Should recieve packages from api.maplink.com.br", (done) => {
+			ping.sys.probe("api.maplink.com.br", (isAlive) => {
+				expect(isAlive).to.be.true;
+				done();
+			});
 		})
 	})
 })
